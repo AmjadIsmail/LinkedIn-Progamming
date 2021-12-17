811f7e778c37d50f3e660e308ee885e0ef30baf8
= <=>
2021-12-17 07:16:35 +0000

    Birth Day Cake Candles Max Number count
    Birth Day Cake Candles Max Number count

---
+++ Hacker_Rank/HackerRank-Problems/01-BirthdayCake.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Problems
+{
+    public class BirthdayCake_Result
+    {
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int Count = 0;
+            int[] ArrCancel = candles.ToArray();
+            int maxValue = ArrCancel.Max();
+            for (int j = 0; j < ArrCancel.Length; j++)
+            {
+                if (ArrCancel[j] == maxValue) { Count++; }
+            }
+            return Count;
+
+        }
+    }
+
+    public class BirthdayCake_Solution
+    {
+        public static void Input(List<int> candles) 
+        {
+           // int[] arr = new int[] { 1, 2, 3, };
+            BirthdayCake_Result.birthdayCakeCandles(candles);
+        }
+    }
+
+
+}
---
+++ Hacker_Rank/HackerRank-Problems/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Problems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Prob-1 BirthdayCake
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            int result = BirthdayCake_Result.birthdayCakeCandles(candles);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+          
+            #endregion
+        }
+    }
+}
